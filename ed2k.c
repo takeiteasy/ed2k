4693f767519a6eeb9314a3f8a38a9a4790b9eae4
Rusty Shackleford <gigolo@hotmail.co.uk>
2016-11-13 18:11:56 +0000

    worked in queues

--- ed2k.c
+++ ed2k.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
 #include <openssl/md4.h>
-#include "threads/threads.h"
+#include "queue_t/queue.h"
 
 #define CHUNK_SIZE 9728000
 #define BUF_SIZE   8000
+#define THREADS    4
 
-void* ed2k() {
+void* ed2k(void* arg) {
+  queue_t* q = (queue_t*)arg;
+  queue_node_t* qn;
+  while (q->nodes) {
+    qn = queue_get(q);
+    printf("%s\n", (char*)qn->data);
+    thrd_yield();
+    free(qn);
+  }
+  thrd_exit(0);
 }
 
 int main (int argc, const char *argv[]) {
-  long nprocessors = sysconf(_SC_NPROCESSORS_ONLN);
+  queue_t* q = queue_init();
+  for (int i = 1; i < argc; ++i)
+    queue_add(q, argv[i], sizeof(argv[i]));
 
-  thrd_t* t = malloc(nprocessors * sizeof(thrd_t*));
+  thrd_t* t = malloc(THREADS * sizeof(thrd_t*));
+  for (int i = 0; i < THREADS; ++i)
+    thrd_create(&t[i], ed2k, (void*)q);
+  for (int i = 0; i < THREADS; ++i)
+    thrd_join(t[i], NULL);
+
   return 0;
 }
