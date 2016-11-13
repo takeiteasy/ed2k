5115437c8271984f6e9a7f620f6410fb2f219516
Rusty Shackleford <gigolo@hotmail.co.uk>
2016-11-13 22:51:29 +0000

    fixed bus error

--- test.c
+++ test.c
@@ -14,6 +14,7 @@
 
 typedef struct {
   int id, fh;
+  size_t  fs;
   queue_t* q;
 } worker_arg;
 
@@ -22,26 +23,27 @@ typedef struct {
   int    id;
 } chunk_t;
 
-void worker(void* arg) {
+void hasher(void* arg) {
   worker_arg* a = (worker_arg*)arg;
   queue_node_t* qn;
   while (a->q->nodes) {
     qn = queue_get(a->q);
     chunk_t* chunk = (chunk_t*)qn->data;
-    char* data = mmap(0, CHUNK_SIZE, PROT_READ, MAP_SHARED, a->fh, chunk->offset);
+    size_t data_size = (chunk->offset + CHUNK_SIZE > a->fs ? a->fs - chunk->offset : CHUNK_SIZE);
+    char* data = mmap(0, data_size, PROT_READ, MAP_SHARED, a->fh, chunk->offset);
     if (data == MAP_FAILED)
       exit(-1);
 
     unsigned char md[MD4_DIGEST_LENGTH];
     MD4_CTX root;
     MD4_Init(&root);
-    MD4_Update(&root, data, CHUNK_SIZE);
+    MD4_Update(&root, data, data_size);
     MD4_Final(md, &root);
 
     char* result = malloc(32 * sizeof(char*));
     for (int i = 0; i < MD4_DIGEST_LENGTH; ++i)
       sprintf(&result[i * 2], "%02x", (unsigned int)md[i]);
-    printf("%s %d %d\n", result, a->id, chunk->id);
+    // printf("%s %d %d\n", result, a->id, chunk->id);
 
     thrd_yield();
     free(qn);
@@ -60,12 +62,12 @@ int main() {
   int status = fstat (fh, &s);
   if (status < 0)
     return -1;
-  size_t size = s.st_size;
+  size_t f_size = s.st_size;
 
   queue_t* q = queue_init();
   size_t   i = 0;
   int      j = 0;
-  for (; i < size; i += CHUNK_SIZE) {
+  for (; i < f_size; i += CHUNK_SIZE) {
     chunk_t* chunk = (chunk_t*)malloc(sizeof(chunk_t));
     chunk->offset = i;
     chunk->id     = j++;
@@ -77,8 +79,9 @@ int main() {
     worker_arg* arg = (worker_arg*)malloc(sizeof(worker_arg));
     arg->id = i;
     arg->fh = fh;
+    arg->fs = f_size;
     arg->q  = q;
-    thrd_create(&t[i], worker, (void*)arg);
+    thrd_create(&t[i], hasher, (void*)arg);
   }
   for (int i = 0; i < THREADS; ++i)
     thrd_join(t[i], NULL);
