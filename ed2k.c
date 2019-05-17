725fb7d796aaa8dd3afadfebd2fc807026986db9
Rusty Shackleford <gigolo@hotmail.co.uk>
2019-05-17 11:51:07 +0100

    rewrite

--- ed2k.c
+++ ed2k.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include <openssl/md4.h>
@@ -6,101 +7,160 @@
 
 #define CHUNK_SIZE 9728000
 #define BUF_SIZE   8000
+#if !defined(THREADS)
+#define THREADS 8
+#endif
 
-const char** files;
-int next_file   = 0,
-    total_files = 0;
+typedef struct _q_node {
+  void*  data;
+  size_t size;
+  struct _q_node* next;
+} queue_node_t;
 
-void* ed2k() {
-  int this_file;
-  while (next_file < total_files) {
-    this_file  = next_file;
-    next_file += 1;
-    FILE* fh   = fopen(files[this_file], "rb");
+typedef struct _queue {
+  queue_node_t* nodes;
+  size_t length;
+  pthread_mutex_t mod_lock, read_lock;
+} queue_t;
 
-    if (fh) {
-      fseek(fh, 0L, SEEK_END);
-      size_t fh_size = ftell(fh);
-      rewind(fh);
-      int too_small  = (fh_size < CHUNK_SIZE);
+void queue_init(queue_t* q) {
+  q->length = 0;
+  q->nodes  = NULL;
 
-      unsigned char buf[BUF_SIZE];
-      unsigned char md [MD4_DIGEST_LENGTH];
-      MD4_CTX root;
-      MD4_CTX chunk;
-      MD4_Init(&root);
-      MD4_Init(&chunk);
+  
+  pthread_mutex_init(&q->mod_lock, NULL);
+  pthread_mutex_init(&q->read_lock, NULL);
+  pthread_mutex_lock(&q->read_lock);
+}
 
-      size_t cur_len   = 0,
-             len       = 0,
-             cur_chunk = 0,
-             cur_buf   = 0;
-      while (fread(buf, sizeof(*buf), BUF_SIZE, fh) > 0) {
-        len        = ftell(fh);
-        cur_buf    = len - cur_len;
-        MD4_Update(&chunk, buf, cur_buf);
-        cur_len    = len;
-        cur_chunk += BUF_SIZE;
+void queue_add(queue_t* q, void* _data, size_t _size) {
+  queue_node_t* qn = calloc(1, sizeof(queue_node_t));
+  if (!qn)
+    return;
 
-        if (cur_chunk == CHUNK_SIZE && cur_buf == BUF_SIZE) {
-          cur_chunk = 0;
-          MD4_Final(md, &chunk);
-          MD4_Init(&chunk);
-          MD4_Update(&root, md, MD4_DIGEST_LENGTH);
-        }
-      }
-      MD4_Final(md, &chunk);
+  qn->data = _data;
+  qn->size = _size;
 
-      if (!too_small) {
-        MD4_Update(&root, md, MD4_DIGEST_LENGTH);
-        MD4_Final(md, &root);
-      }
+  pthread_mutex_lock(&q->mod_lock);
+  qn->next = q->nodes;
+  q->nodes = qn;
+  q->length++;
 
-      char* result = malloc(32 * sizeof(char*));
-      int i        = 0;
-      for(; i < MD4_DIGEST_LENGTH; ++i)
-        sprintf(&result[i * 2], "%02x", (unsigned int)md[i]);
-      printf("ed2k://|file|%s|%lu|%s|\n", files[this_file], ftell(fh), result);
+  pthread_mutex_unlock(&q->mod_lock);
+  pthread_mutex_unlock(&q->read_lock);
+}
 
-      free(result);
-      fclose(fh);
-    }
+queue_node_t* queue_get(queue_t* q) {
+  queue_node_t *qn, *pqn;
+
+  pthread_mutex_lock(&q->mod_lock);
+  pthread_mutex_lock(&q->read_lock);
+
+  if (!(qn = pqn = q->nodes)) {
+    pthread_mutex_unlock(&q->mod_lock);
+    return (queue_node_t*)NULL;
   }
-  pthread_exit(NULL);
+
+  qn = pqn = q->nodes;
+  while ((qn->next)) {
+    pqn = qn;
+    qn  = qn->next;
+  }
+
+  pqn->next = NULL;
+  if (q->length <= 1)
+    q->nodes = NULL;
+  q->length--;
+
+  if (q->length > 0)
+    pthread_mutex_unlock(&q->read_lock);
+  pthread_mutex_unlock(&q->mod_lock);
+
+  return qn;
+}
+
+void ed2k(void* _q) {
+  queue_t* q = (queue_t*)_q;
+  queue_node_t* qn;
+  while(q->nodes) {
+    qn = queue_get(q);
+    char* fn = (char*)qn->data;
+    FILE* fh = fopen(fn, "rb");
+    if (!fh) {
+      printf("ERROR: Failed to open \"%s\"\n", fn);
+      free(qn);
+      continue;
+    }
+
+    fseek(fh, 0L, SEEK_END);
+    size_t fh_size = ftell(fh);
+    rewind(fh);
+    int too_small  = (fh_size < CHUNK_SIZE);
+
+    unsigned char buf[BUF_SIZE];
+    unsigned char md [MD4_DIGEST_LENGTH];
+    MD4_CTX root;
+    MD4_CTX chunk;
+    MD4_Init(&root);
+    MD4_Init(&chunk);
+
+    size_t cur_len   = 0,
+           len       = 0,
+           cur_chunk = 0,
+           cur_buf   = 0;
+    while (fread(buf, sizeof(*buf), BUF_SIZE, fh) > 0) {
+      len        = ftell(fh);
+      cur_buf    = len - cur_len;
+      MD4_Update(&chunk, buf, cur_buf);
+      cur_len    = len;
+      cur_chunk += BUF_SIZE;
+
+      if (cur_chunk == CHUNK_SIZE && cur_buf == BUF_SIZE) {
+        cur_chunk = 0;
+        MD4_Final(md, &chunk);
+        MD4_Init(&chunk);
+        MD4_Update(&root, md, MD4_DIGEST_LENGTH);
+      }
+    }
+    MD4_Final(md, &chunk);
+
+    if (!too_small) {
+      MD4_Update(&root, md, MD4_DIGEST_LENGTH);
+      MD4_Final(md, &root);
+    }
+
+    char* result = malloc(32 * sizeof(char*));
+    int i        = 0;
+    for(; i < MD4_DIGEST_LENGTH; ++i)
+      sprintf(&result[i * 2], "%02x", (unsigned int)md[i]);
+    printf("ed2k://|file|%s|%lu|%s|\n", fn, ftell(fh), result);
+
+    free(result);
+    fclose(fh);
+    free(qn);
+  }
 }
 
 int main (int argc, const char *argv[]) {
-  int total_threads = 8, i = 0, j = 0;
-  const char** tmp_args = malloc(argc * sizeof(char*));
-  for (i = 1, j = 0; i < argc; ++i) {
-    if (argv[i][0] == '-' && argv[i][1] == '-') {
-      if (!strcmp("--threads", argv[i])) {
-        if ((i + 1) > (argc - 1))
-          continue;
-
-        total_threads = (unsigned)atoi(argv[++i]);
-        if (total_threads <= 1)
-          total_threads = 1;
-      }
-    } else {
-      tmp_args[j] = malloc(strlen(argv[i]) * sizeof(char*));
-      strcpy(tmp_args[j], argv[i]);
-      j++;
-    }
+  int i, total_threads = THREADS, total_files = argc - 1;
+  if (!total_files) {
+    printf("Nothing to do!\n");
+    return 1;
   }
-
-  total_files = j;
   if (total_files < total_threads)
     total_threads = total_files;
-  files       = malloc(j * sizeof(char*));
-  memcpy(files, tmp_args, j * sizeof(char*));
-  free(tmp_args);
+  queue_t q;
 
+  queue_init(&q);
+  for (i = 1; i < argc; ++i)
+    queue_add(&q, (void*)argv[i], sizeof(argv[i]));
+  
   pthread_t threads[total_threads];
   for (i = 0; i < total_threads; ++i)
-    pthread_create(&threads[i], NULL, ed2k, NULL);
+    pthread_create(&threads[i], NULL, ed2k, (void*)&q);
   for (i = 0; i < total_threads; ++i)
     pthread_join(threads[i], NULL);
 
+  free(q.nodes);
   return 0;
 }
